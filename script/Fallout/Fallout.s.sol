74b40e2a1302a52cdc8a538a36eb4ae1f518725b
eieueo <eieueo@eieueos-MacBook-Pro.local>
2023-01-23 16:25:34 +0700

    solved Fallout

--- script/Fallout/Fallout.s.sol
+++ script/Fallout/Fallout.s.sol
@@ -20,7 +20,7 @@ contract FalloutScript is Script {
             /** Define addresses  (NO NEED TO CHANGE ANYTHING HERE) **/
             attacker = msg.sender;
             /** Setup contract and required init (you may have to modify this section) **/
-            target = Fallout(0x4ae3BD614bb543B373fdc271797ed4Abd48C53Aa); //attach to an existing contract: 0x0000000000000000000000000000000000000000
+            target = Fallout(0x0000000000000000000000000000000000000000); //attach to an existing contract
         }else{ // local - chainid = 31137
             /** Define actors (NO NEED TO CHANGE ANYTHING HERE) **/
             deployer = vm.addr(1);
